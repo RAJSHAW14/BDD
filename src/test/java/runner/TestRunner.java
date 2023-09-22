3152de9f09e3ebfed37f254f3b228a2a4b48d6a5
RAJSHAW14 <rajshaw_14@outlook.com>
2023-09-23 00:31:23 +0530

    Test Runner Updated

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/Resources/Features/Register.feature",
+        features = "src/test/Resources/Features",
         glue = {"stepdefinition","hooks"},
         monochrome = true,
         dryRun = false,
